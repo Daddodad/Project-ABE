2474400bde216281304a96bfa65759ce6c6f5edf
Daddodad <dav.moretti2@stud.uniroma3.it>
2023-01-19 15:19:42 +0100

    Fist commit

---
+++ build/CMakeFiles/cpa-abe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpa-abe.dir/src/cpa-abe.cpp.o"
+  "cpa-abe"
+  "cpa-abe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpa-abe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
