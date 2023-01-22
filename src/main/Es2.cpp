f8085a7ab7a68526ee2a9aec3b06360fcb795669
Daddodad <dav.moretti2@stud.uniroma3.it>
2023-01-22 11:49:35 +0100

    22-01

--- src/main/Es2.cpp
+++ src/main/Es2.cpp
@@ -16,6 +16,9 @@
 #include "math/distrgen.h"
 #include "utils/inttypes.h"
 
+#include <iostream>
+#include <fstream>
+
 using namespace lbcrypto;
 int main() {
 
@@ -34,24 +37,44 @@ CPABEMasterSecretKey<NativePoly> msk;
 
 contesto.Setup(&mpk, &msk);
 
-    size_t rows=5;
-    size_t cols=5;
-    Matrix<int64_t> test([]() { return 0; }, rows, cols);
+std::cout << mpk.GetA().ExtractCol(0);
+
+NativePoly pippo=contesto.GenerateRandomElement();
+NativePoly franco;
+//std::cout << pippo[0];
+//std::cout << pippo.SwitchFormat();
+
+    //size_t rows=5;
+    //size_t cols=5;
+    Matrix<NativePoly> test;
     std::cout << test.SerializedObjectName();
     //auto zero_alloc = NativePoly::Allocator(5, Format::EVALUATION);
     //test.Matrix();
 
     //test.SetSize(rows,cols);
     
+std::fstream my_file;
+my_file.open("../src/files/A2.txt", std::ios::out);
+    //size_t zero=1;
+	if (!my_file) {
+		std::cout << "File not created!\n";
+	}
+	else {
+		std::cout << "File created successfully!\n";    
+        my_file << test;
+		my_file.close(); 
+}
+//test=mpk.GetA();
+//test.HStack(mpk.GetA().ExtractCol(zero));
     
-    std::fstream my_file;
-    my_file.open("../src/files/mtest.txt", std::ios::out);
+    //std::fstream my_file;
+    my_file.open("../src/files/A2", std::ios::in);
     if (!my_file) {
 		std::cout << "File not opened!\n";
 	}
 	else {
 		std::cout << "File opened successfully!\n";
-        my_file << test.ExtractRow(rows-1);
+        //my_file >> franco;
 		my_file.close(); 
 	}
 
